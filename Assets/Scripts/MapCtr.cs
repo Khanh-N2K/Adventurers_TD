c544cd73fccbfafa8c8d45ecb4e00872dc33ad49
LuongSon28112004 <sonluong165@gmail.com>
2026-01-23 21:39:12 +0700

    spawn enemy anf character

--- Assets/Scripts/MapCtr.cs
+++ Assets/Scripts/MapCtr.cs
@@ -1,4 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MapCtr : Singleton<MapCtr>
@@ -9,13 +12,99 @@ public class MapCtr : Singleton<MapCtr>
     public List<Base> listCharacters = new();
     public Transform characterTransform;
     public Transform enemyTransform;
+    private int currentWave = 0;
+    public SpawnSO spawnSOData;
+
+    private void Start() {
+        StartCoroutine(SpawnEnemyWave(0));
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            SpawnCharacter(0);
+        }
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            SpawnCharacter(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            SpawnCharacter(2);
+        }
+    }
+
+    public void StartSpawn()
+    {
+        
+    }
     public bool CheckEndWave()
     {
         return true;
     }
-    public void SpawnCharacter(int idx)
+    public void SpawnCharacter(int id)
     {
-        
+        Base Character = CreateCharacterObject(id, characterTransform);
+        listCharacters.Add(Character);
+    }
+
+
+    private Base GetEnemyById(int id)
+    {
+        for(int i = 0 ; i < enemyPrefab.Length ; i++)
+        {
+            if(enemyPrefab[i].info.id == id)
+            {
+                return enemyPrefab[i];
+            }
+        }
+        return null;
+    }
+
+    private Base GetCharacterById(int id)
+    {
+        for(int i = 0 ; i < characterPrefab.Length ; i++)
+        {
+            if(characterPrefab[i].info.id == id)
+            {
+                return characterPrefab[i];
+            }
+        }
+        return null;
+    }
+
+    public IEnumerator SpawnEnemyWave(int index)
+    {
+        InfoWave infoWave = spawnSOData.infoSpawn.infoWave[index];
+        yield return new WaitForSeconds(infoWave.timeDelay - 13f);
+        for(int i = 0 ; i < infoWave.infoTurns.Length ; i++)
+        {
+            InfoTurn infoTurn = infoWave.infoTurns[i];
+            for(int j = 0 ; j < infoTurn.enemy.Length ; j++)
+            {
+                Base enemy = CreateEnemyObject(infoTurn.enemy[j] , enemyTransform);
+                listEnemys.Add(enemy);
+            }
+            yield return new WaitForSeconds(infoTurn.timedelay);
+        }
+    }
+    
+
+    public Base CreateEnemyObject(int id, Transform parent)
+    {
+        Base enemy = GetEnemyById(id);
+        Instantiate(enemy, parent.transform.position + new Vector3(UnityEngine.Random.Range(-0.05f,0.05f),0.03f,UnityEngine.Random.Range(-0.05f,0.05f)), Quaternion.identity, parent);
+        return enemy;
+    }
+
+    public Base CreateCharacterObject(int id, Transform parent)
+    {
+        Base Character = GetCharacterById(id);
+        Instantiate(Character, parent.transform.position + new Vector3(UnityEngine.Random.Range(-0.05f,0.05f),0.03f,UnityEngine.Random.Range(-0.05f,0.05f)), Quaternion.identity, parent);
+        return Character;
     }
 
 }
