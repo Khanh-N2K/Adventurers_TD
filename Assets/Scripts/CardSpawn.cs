4ce60a017348281c82bb64390dc340ec6c8f9f0e
Huy Nup <minecraftvipking137@gmail.com>
2026-01-23 20:28:33 +0700

    Theem Spawn Card

--- Assets/Scripts/CardSpawn.cs
+++ Assets/Scripts/CardSpawn.cs
@@ -1,25 +1,66 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class CardSpawn : MonoBehaviour
+public class CardSpawn : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+    public Vector2 startScale = Vector2.one;
+    public Vector2 endScale = new Vector2(0.9f, 0.9f);
+    private bool isPointerDown = false;
     public bool isClick;
     public float countdown;
     public Button btn;
     public Image fill;
+    public int coin;
+    public Text text;
     public Action onClick;
+    private bool isCoin;
     public void Start()
     {
-        isClick = false;
+        isClick = true;
+        fill.fillAmount = 0;
+        text.text = coin.ToString();
         btn.onClick.AddListener(OnClick);
+        isCoin = coin <= UIInGame.Instance.Coin;
     }
     public void OnClick()
     {
+        if (!isClick || !isCoin) return;
+        isClick = false;
+        btn.enabled = false;
         onClick?.Invoke();
+        fill.fillAmount = 1;
+        fill.DOFillAmount(0, countdown).OnComplete(() =>
+        {
+            isClick = true;
+            btn.enabled = true;
+            fill.fillAmount = 0;
+        });
+        UIInGame.Instance.RemoveCoin(coin);
+    }
+    public void CheckClickButton()
+    {
+        isCoin = coin <= UIInGame.Instance.Coin;
+        btn.interactable = isCoin;
     }
     public void AddListener(Action action)
     {
         onClick = action;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (!isClick || !isCoin) return;
+        isPointerDown = true;
+        transform.DOScale(endScale, 0.1f).SetEase(Ease.Linear).SetUpdate(true).SetId(this);
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!isClick || !isCoin) return;
+        if (!isPointerDown) return;
+        isPointerDown = false;
+        transform.DOScale(startScale, 0.1f).SetEase(Ease.Linear).SetUpdate(true).SetId(this);
     }
 }
--- Assets/Scripts/UIInGame.cs
+++ Assets/Scripts/UIInGame.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIInGame : Singleton<UIInGame>
 {
     public int Coin = 50;
+    public Text coinTxt;
     public CardSpawn[] listCards;
     public void Start()
     {
+        coinTxt.text = Coin.ToString();
         for (int i = 0; i < listCards.Length; i++)
         {
             listCards[i].AddListener(() => SpawnCharacter(i));
@@ -15,5 +18,48 @@ public class UIInGame : Singleton<UIInGame>
     public void SpawnCharacter(int idx)
     {
         MapCtr.Instance.SpawnCharacter(idx);
+    }
+    public void AddCoin(int amount)
+    {
+        int startValue = Coin;
+        int endValue = Coin + amount;
+
+        Coin = endValue;
+        CheckClickListCard();
+        DOTween.To(
+            () => startValue,
+            x =>
+            {
+                startValue = x;
+                coinTxt.text = x.ToString();
+            },
+            endValue,
+            0.5f
+        ).SetEase(Ease.OutQuad).OnComplete(() => { coinTxt.text = Coin.ToString(); });
+    }
+    public void RemoveCoin(int amount)
+    {
+        int startValue = Coin;
+        int endValue = Mathf.Max(0, Coin - amount);
+
+        Coin = endValue;
+        CheckClickListCard();
+        DOTween.To(
+            () => startValue,
+            x =>
+            {
+                startValue = x;
+                coinTxt.text = x.ToString();
+            },
+            endValue,
+            0.5f
+        ).SetEase(Ease.InQuad).OnComplete(() => { coinTxt.text = Coin.ToString(); });
+    }
+    public void CheckClickListCard()
+    {
+        foreach (CardSpawn card in listCards)
+        {
+            card.CheckClickButton();
+        }
     }
 }
